7fcf91fd1638328e3a10fac13fd21d09a6bc258e
JamesBremner <james@ravenspoint.com>
2024-08-13 11:59:45 -0400

    code documentation

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,15 @@ struct sdeform
     {
     }
 };
+
+/// @brief  A pile of string
+
 struct spile
 {
     std::vector<cxy> vnode;    // initial node locations
     std::vector<cxy> vcurrent; // current node locations
-    std::vector<std::pair<int, int>> vedge;
-    std::vector<sdeform> vdeform;
+    std::vector<std::pair<int, int>> vedge; // edges between nodes
+    std::vector<sdeform> vdeform;   // define deformations 
 
     void clear()
     {
@@ -34,6 +37,7 @@ struct spile
         vedge.clear();
         vdeform.clear();
     }
+    /// @brief  Perform deformation
     void deform()
     {
         if (!vcurrent.size())
@@ -41,9 +45,13 @@ struct spile
             vcurrent = vnode;
             return;
         }
+
+        // TODO: implement more than one node moving
         vcurrent[vdeform[0].node].x += vdeform[0].xv;
         vcurrent[vdeform[0].node].y += vdeform[0].yv;
     }
+
+    // Reverse the last deformation
     void deformReverse()
     {
         if (!vcurrent.size())
@@ -54,12 +62,16 @@ struct spile
         vcurrent[vdeform[0].node].x -= vdeform[0].xv;
         vcurrent[vdeform[0].node].y -= vdeform[0].yv;
     }
+    /// @brief identify the edges that are moving
+    /// @return vector of indices of moving edges
     std::vector<int>
     moving()
     {
         std::vector<int> ret;
         for (int iedge = 0; iedge < vedge.size(); iedge++)
         {
+            // check if edge includes a moving node
+            // TODO: implement more then one moving node
             if (vedge[iedge].first == vdeform[0].node ||
                 vedge[iedge].second == vdeform[0].node)
                 ret.push_back(iedge);
@@ -68,28 +80,37 @@ struct spile
     }
     bool isIntersection()
     {
+        // maximum distance squared that will be ignored
         const double delta = 0.01;
 
         cxy p;
+
+        // loop over moving edges
         for (int iedgemoving : moving())
         {
             auto n1me = vcurrent[vedge[iedgemoving].first];
             auto n2me = vcurrent[vedge[iedgemoving].second];
+
+            // loop over fixed edges
             for (int iedgefixed = 0; iedgefixed < vedge.size(); iedgefixed++)
             {
                 if (iedgefixed == iedgemoving)
                     continue;
                 auto n1fe = vcurrent[vedge[iedgefixed].first];
                 auto n2fe = vcurrent[vedge[iedgefixed].second];
+
+                // check for intersection
                 if (cxy::isIntersection( p,
                         n1me, n2me,
                         n1fe,n2fe))
                 {
+                    // check that intesection NOT at a shared node
                     if( ! ( n1me.dist2(p) < delta || n2me.dist2(p) < delta ) )
                         return true;
                 }
             }
         }
+        // no edges intersect
         return false;
     }
 
@@ -129,7 +150,22 @@ ParseSpaceDelimited(
 
     return token;
 }
-
+/**
+ * @brief read input
+ * 
+ * @param fname input file path
+ * 
+ * one line per node, giving its initial location
+ * n x y
+ * 
+ * one line per edge connecting nodes
+ * e node_index1 node_index2
+ * 
+ * one line per moving node
+ * d node_index x_direction y_direction velocity
+ * 
+ * Note only one moving node implemented
+ */
 void read(const std::string fname)
 {
     thePile.clear();
@@ -183,14 +219,22 @@ main()
 {
     //cGUI theGUI;
 
+    // read input
     read( "../dat/data1.txt");
+
+    // repeatedly apply deformation
     for( ; ; ) {
+
         thePile.deform();
+
         if( thePile.isIntersection() ) {
+            // two edges have intersected, back up and finish
             thePile.deformReverse();
             break;
         }
     }
+
+    // display initial and final node positions
     thePile.text();
     return 0;
 }
